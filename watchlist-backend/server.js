e259add6a7af4c839f2da37179ad5e5889e8b0cf
AlexaSantana <ac9742793@gmail.com>
2026-04-22 16:05:52 -0400

    update server.js

--- watchlist-backend/server.js
+++ watchlist-backend/server.js
@@ -6,8 +6,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let peliculas = [];
+
+app.post("/peliculas", (req, res) => {
+  const peli = req.body;
+
+  if (!peli.titulo || !peli.genero) {
+    return res.status(400).json({ error: "Faltan datos" });
+  }
+
+  peliculas.push(peli);
+  res.json({ mensaje: "Película guardada", data: peli });
+});
+
+app.get("/peliculas", (req, res) => {
+  res.json(peliculas);
+});
+
+app.put("/peliculas/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!peliculas[id]) {
+    return res.status(404).json({ error: "No existe" });
+  }
+
+  peliculas[id] = req.body;
+  res.json({ mensaje: "Actualizada" });
+});
+
+app.delete("/peliculas/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!peliculas[id]) {
+    return res.status(404).json({ error: "No existe" });
+  }
+
+  peliculas.splice(id, 1);
+  res.json({ mensaje: "Eliminada" });
+});
+
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
+  res.send("Servidor funcionando");
 });
 
 const PORT = 3000;
