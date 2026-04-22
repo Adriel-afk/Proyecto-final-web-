4f6b25e3a9de090b1e65fede6fc56ed73cf573cc
AlexaSantana <ac9742793@gmail.com>
2026-04-22 17:22:51 -0400

    update server.js

--- watchlist-backend/server.js
+++ watchlist-backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.use(cors());
