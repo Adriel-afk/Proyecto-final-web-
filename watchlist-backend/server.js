86631c8a30cb9a0e02bce9eb84c7867ffa4b2f78
AlexaSantana <ac9742793@gmail.com>
2026-04-23 15:53:42 -0400

    update server.js

--- watchlist-backend/server.js
+++ watchlist-backend/server.js
@@ -68,3 +68,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor corriendo en http://localhost:" + PORT);
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
